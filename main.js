a46c86fdcedb69ce8a81dc23f4aef5391ffc8078
MakedonskyVladimir <shurmanoff@mail.ru>
2023-01-10 09:24:10 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const questions = [
+    {
+        question: "Какой язык работает в браузере?", 
+        answer: ["Java", "C", "Python", "JavaScript"], 
+        correct: 4, 
+    },
+    {
+        question: "Что означает CSS?", 
+        answer: [
+            "Central Style Sheets", 
+            "Cascading Style Sheets", 
+            "Cascading Simple Sheets", 
+            "Cars SUVs SailBoats"], 
+        correct: 2, 
+    },
+    {
+        question: "Что означает HTML?", 
+        answer: [
+            "Hypertext Markup Language", 
+            "Hypertext Marcdown Language", 
+            "Hyperloop Machine Language", 
+            "Helicopters Terminal Motorboats Lamborginis"], 
+        correct: 1, 
+    },
+    {
+        question: "В каком году был создан JavaScript?", 
+        answer: ["1996", "1995", "1994", "все ответы верные"], 
+        correct: 4, 
+    },
+    
+]
